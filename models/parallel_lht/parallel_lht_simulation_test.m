ee11c2cbccd4f780c70089d1c3ebfa033f7a74f9
dnorthcote <42978178+dnorthcote@users.noreply.github.com>
2020-02-27 19:03:02 +0000

    Added better testing for simulation with multiple image support

--- models/parallel_lht/parallel_lht_simulation_test.m
+++ models/parallel_lht/parallel_lht_simulation_test.m
@@ -3,64 +3,69 @@ clear;
 close all;
 
 %% Setup the names of tests we are going to complete
-name = {'1080p', '1024p', '720p', '768p', '480p', '512o', '333o', '240p'};
+name = {'1080p', '1024p', '720p', '768p', '480p', '512_Chen', '333_Zhou' '240p'};
 status = ones(1, length(name)); % status vector all set to one which indicates fail.
 
 %% Load a test image
-I = imread('brick_wall.jpg');
+I = {imread('brick_wall.jpg'), imread('circle_steps_twist.jpg')};
+img_status = ones(1, length(I)); % status vector all set to one which indicates fail.
 
 %% Open the model
 open_system('parallel_lht');
 
 %% Iterate through each test
 for name_idx = 1:length(name)
-    load([name{name_idx}, '_BaseTest.mat']);
-    
-    % Image Initialisation
-    Y = double(imresize(rgb2gray(I), [bt.height, bt.width]));
+    for img_idx = 1:length(I)
+        load([name{name_idx}, '_BaseTest.mat']);
 
-    % Apply the Sobel operators to the greyscale image to obtain the binary
-    % edge image and gradient orientation.
-    SobelThreshold = 30;
-    [edge, ~] = Sobel(Y, SobelThreshold);
+        % Image Initialisation
+        Y = double(imresize(rgb2gray(I{img_idx}), [bt.height, bt.width]));
 
-    % Set input image
-    bt.inputImage = uint8(edge);
+        % Apply the Sobel operators to the greyscale image to obtain the binary
+        % edge image and gradient orientation.
+        SobelThreshold = 30;
+        [edge, ~] = Sobel(Y, SobelThreshold);
 
-    % Run the simulation
-    RunParallelLHT(bt);
-    
-    % Get hough parameter space
-    Hparam = zeros(bt.height, bt.width, bt.nTheta);
-    HPS = zeros(bt.nRho, bt.nTheta);
+        % Set input image
+        bt.inputImage = uint8(edge);
 
-    % Get Quantised Trig
-    cosQ = double(fi(cos(deg2rad(bt.theta)), 1, bt.paramWord, bt.paramWord-2));
-    sinQ = double(fi(sin(deg2rad(bt.theta)), 1, bt.paramWord, bt.paramWord-2));
+        % Run the simulation
+        RunParallelLHT(bt);
 
-    for y = 1:bt.height
-        for x = 1:bt.width
-            if (bt.inputImage(y, x))
-                yTemp = y - floor(bt.height/2) -1;
-                xTemp = x - floor(bt.width/2) -1;
-                % This calculation will need to be changed when using the Basic
-                % models that do not round/truncate
-                rhoTemp = round(round(xTemp*cosQ) + round(yTemp*sinQ) + bt.maxRho);
-                Hparam(y, x, :) = rhoTemp;
-                for i = 1: bt.nTheta
-                    HPS(rhoTemp(i) + 1, i) = HPS(rhoTemp(i) + 1, i) + 1;
+        % Get hough parameter space
+        Hparam = zeros(bt.height, bt.width, bt.nTheta);
+        HPS = zeros(bt.nRho, bt.nTheta);
+
+        % Get Quantised Trig
+        cosQ = double(fi(cos(deg2rad(bt.theta)), 1, bt.paramWord, bt.paramWord-2));
+        sinQ = double(fi(sin(deg2rad(bt.theta)), 1, bt.paramWord, bt.paramWord-2));
+
+        for y = 1:bt.height
+            for x = 1:bt.width
+                if (bt.inputImage(y, x))
+                    yTemp = y - floor(bt.height/2) -1;
+                    xTemp = x - floor(bt.width/2) -1;
+                    % This calculation will need to be changed when using the Basic
+                    % models that do not round/truncate
+                    rhoTemp = round(round(xTemp*cosQ) + round(yTemp*sinQ) + bt.maxRho);
+                    Hparam(y, x, :) = rhoTemp;
+                    for i = 1: bt.nTheta
+                        HPS(rhoTemp(i) + 1, i) = HPS(rhoTemp(i) + 1, i) + 1;
+                    end
                 end
             end
         end
-    end
 
-    % Get simulation hough parameter space
-    hpsOutSim = double(reshape(out.simout, [bt.nRho bt.nTheta]));
-    hpsDiff = HPS-hpsOutSim;
-    max(max(abs(hpsDiff)));
+        % Get simulation hough parameter space
+        hpsOutSim = double(reshape(out.simout, [bt.nRho bt.nTheta]));
+        hpsDiff = HPS-hpsOutSim;
+        max(max(abs(hpsDiff)));
+
+        % Set status
+        img_status(img_idx) = ~(max(max(abs(hpsDiff))) == 0);
+    end
     
-    % Set status
-    status(name_idx) = ~(max(max(abs(hpsDiff))) == 0);
+    status(name_idx) = max(img_status);
 end
 
 % Write results to file
